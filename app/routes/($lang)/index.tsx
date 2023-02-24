ee7280b266b2cf8424718ad3412d981a4963521f
Natalie Oldroyd <natalie.oldroyd@shopify.com>
2023-02-23 21:15:11 -0330

    feature image

--- app/routes/($lang)/index.tsx
+++ app/routes/($lang)/index.tsx
@@ -1,36 +1,12 @@
-import {defer, type LoaderArgs} from '@shopify/remix-oxygen';
+import {defer} from '@shopify/remix-oxygen';
 import {Suspense} from 'react';
 import {Await, useLoaderData} from '@remix-run/react';
 import {ProductSwimlane, FeaturedCollections, Hero} from '~/components';
 import {MEDIA_FRAGMENT, PRODUCT_CARD_FRAGMENT} from '~/data/fragments';
 import {getHeroPlaceholder} from '~/lib/placeholders';
-import type {
-  CollectionConnection,
-  Metafield,
-  ProductConnection,
-} from '@shopify/hydrogen/storefront-api-types';
 import {AnalyticsPageType} from '@shopify/hydrogen';
 
-interface HomeSeoData {
-  shop: {
-    name: string;
-    description: string;
-  };
-}
-
-export interface CollectionHero {
-  byline: Metafield;
-  cta: Metafield;
-  handle: string;
-  heading: Metafield;
-  height?: 'full';
-  loading?: 'eager' | 'lazy';
-  spread: Metafield;
-  spreadSecondary: Metafield;
-  top?: boolean;
-}
-
-export async function loader({params, context}: LoaderArgs) {
+export async function loader({params, context}) {
   const {language, country} = context.storefront.i18n;
 
   if (
@@ -42,59 +18,50 @@ export async function loader({params, context}: LoaderArgs) {
     throw new Response(null, {status: 404});
   }
 
-  const {shop, hero} = await context.storefront.query<{
-    hero: CollectionHero;
-    shop: HomeSeoData;
-  }>(HOMEPAGE_SEO_QUERY, {
-    variables: {handle: 'freestyle'},
+  const {shop, hero} = await context.storefront.query(HOMEPAGE_SEO_QUERY, {
+    variables: {handle: 'frontpage'},
   });
+  console.log(hero)
 
   return defer({
     shop,
     primaryHero: hero,
     // These different queries are separated to illustrate how 3rd party content
     // fetching can be optimized for both above and below the fold.
-    featuredProducts: context.storefront.query<{
-      products: ProductConnection;
-    }>(HOMEPAGE_FEATURED_PRODUCTS_QUERY, {
-      variables: {
-        /**
-         * Country and language properties are automatically injected
-         * into all queries. Passing them is unnecessary unless you
-         * want to override them from the following default:
-         */
-        country,
-        language,
-      },
-    }),
-    secondaryHero: context.storefront.query<{hero: CollectionHero}>(
-      COLLECTION_HERO_QUERY,
+    featuredProducts: context.storefront.query(
+      HOMEPAGE_FEATURED_PRODUCTS_QUERY,
       {
         variables: {
-          handle: 'backcountry',
+          /**
+           * Country and language properties are automatically injected
+           * into all queries. Passing them is unnecessary unless you
+           * want to override them from the following default:
+           */
           country,
           language,
         },
       },
     ),
-    featuredCollections: context.storefront.query<{
-      collections: CollectionConnection;
-    }>(FEATURED_COLLECTIONS_QUERY, {
+    secondaryHero: context.storefront.query(COLLECTION_HERO_QUERY, {
+      variables: {
+        handle: 'backcountry',
+        country,
+        language,
+      },
+    }),
+    featuredCollections: context.storefront.query(FEATURED_COLLECTIONS_QUERY, {
       variables: {
         country,
         language,
       },
     }),
-    tertiaryHero: context.storefront.query<{hero: CollectionHero}>(
-      COLLECTION_HERO_QUERY,
-      {
-        variables: {
-          handle: 'winter-2022',
-          country,
-          language,
-        },
+    tertiaryHero: context.storefront.query(COLLECTION_HERO_QUERY, {
+      variables: {
+        handle: 'winter-2022',
+        country,
+        language,
       },
-    ),
+    }),
     analytics: {
       pageType: AnalyticsPageType.home,
     },
@@ -108,7 +75,7 @@ export default function Homepage() {
     tertiaryHero,
     featuredCollections,
     featuredProducts,
-  } = useLoaderData<typeof loader>();
+  } = useLoaderData();
 
   // TODO: skeletons vs placeholders
   const skeletons = getHeroPlaceholder([{}, {}, {}]);
@@ -205,7 +172,7 @@ const COLLECTION_CONTENT_FRAGMENT = `#graphql
         ...Media
       }
     }
-    spreadSecondary: metafield(namespace: "hero", key: "spread_secondary") {
+    spreadSecondary: metafield(namespace: "hero", key: "") {
       reference {
         ...Media
       }
